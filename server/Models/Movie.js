65448d42ccb5fc6025dfd1c087ae4163a3b9151c
DataNick <DataNick@users.noreply.github.com>
2016-01-30 20:10:12 -0500

    Created movie schema and assigned to module exports

--- server/Models/Movie.js
+++ server/Models/Movie.js
@@ -1,1 +1,18 @@
-var mongoose = require('mongoose'); //gives us an interface into mongodb
+var mongoose = require('mongoose'); //gives us an interface into mongodb AND provide models to mongodb and maps directly to database
+
+//Create the MovieSchema
+//Define all the properties for model object
+var MovieSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true
+  },
+  url: {
+    type: String,
+    required: true
+  }
+});
+
+//Export the model schema
+//similar to requiring mongoose up at the top, when creating a component in NodeJS, it must be assigned to module exports b/c whoever requires it, the module/component is what we are passing back to them
+module.exports = MovieSchema;
