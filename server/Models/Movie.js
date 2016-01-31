bb23a7e931ddf0cfa71164e7cd0be68cd187b660
DataNick <DataNick@users.noreply.github.com>
2016-01-30 20:51:25 -0500

    Assigned MovieSchema to module.exports

--- server/Models/Movie.js
+++ server/Models/Movie.js
@@ -14,5 +14,8 @@ var MovieSchema = new mongoose.Schema({
 });
 
 //Export the model schema
+//Defining schema for the database
 //similar to requiring mongoose up at the top, when creating a component in NodeJS, it must be assigned to module exports b/c whoever requires it, the module/component is what we are passing back to them
+//Have MovieSchema and exported it to the model
+//REMEMBER - SKINNY MODELS, FAT CONTROLLERS
 module.exports = MovieSchema;
