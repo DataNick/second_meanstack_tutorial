7033991e4f21420a5184c00cc3e8d35355a48384
DataNick <DataNick@users.noreply.github.com>
2016-01-31 23:09:00 -0500

    Created movie route

--- server/routes.js
+++ server/routes.js
@@ -1,5 +1,5 @@
 //routes.js assigns a controller to a given route in the app
 
 module.exports = {
-  './movie' : require('./controllers/MovieController')
+  '/movie' : require('./controllers/MovieController')
 };
