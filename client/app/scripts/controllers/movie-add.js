9281cd4e3df809a44cdc0aafdc156a00550cc9f2
DataNick <DataNick@users.noreply.github.com>
2016-02-02 14:02:13 -0500

    Set up controller to handle adding movies

--- client/app/scripts/controllers/movie-add.js
+++ client/app/scripts/controllers/movie-add.js
@@ -8,6 +8,8 @@
  * Controller of the clientApp
  */
  //Movie in the parameter is Restangular movie object
+ //$scope.movie changes as you type into the input fields b/c of two-way binding concept
+ //saveMove - call restangular movie object, post that object to the url, then promises feature that calls this callback function to change the url to go to /movies
 angular.module('clientApp')
   .controller('MovieAddCtrl', function ($scope, Movie, $location) {
     $scope.movie = {};
