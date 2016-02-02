74ec858261e33aaac8d10c73961a5cf46fa0bcfe
DataNick <DataNick@users.noreply.github.com>
2016-02-02 13:49:05 -0500

    Set up movie add controller to add movies

--- client/app/scripts/controllers/movie-add.js
+++ client/app/scripts/controllers/movie-add.js
@@ -7,11 +7,13 @@
  * # MovieAddCtrl
  * Controller of the clientApp
  */
+ //Movie in the parameter is Restangular movie object
 angular.module('clientApp')
-  .controller('MovieAddCtrl', function () {
-    this.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
+  .controller('MovieAddCtrl', function ($scope, Movie, $location) {
+    $scope.movie = {};
+    $scope.saveMovie = function () {
+      Movie.post($scope.movie).then(function () {
+        $location.path('/movies');
+      });
+    };
   });
