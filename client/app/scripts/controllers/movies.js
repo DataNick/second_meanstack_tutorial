e04f500430960e5d674bbe8328826e01cbad09ff
DataNick <DataNick@users.noreply.github.com>
2016-02-02 14:04:52 -0500

    Cleaned up syntax

--- client/app/scripts/controllers/movies.js
+++ client/app/scripts/controllers/movies.js
@@ -8,6 +8,9 @@
  * Controller of the clientApp
  */
 angular.module('clientApp')
-  .controller('MoviesCtrl', function ($scope, Movie) {
+  .controller('MoviesCtrl', function (
+    $scope,
+    Movie
+    ) {
     $scope.movies = Movie.getList().$object;
   });
