e3b95ab6754b0c54a34758ac24afa4c09b634916
DataNick <DataNick@users.noreply.github.com>
2016-02-02 00:53:41 -0500

    Set up binding between  and

--- client/app/scripts/controllers/movies.js
+++ client/app/scripts/controllers/movies.js
@@ -8,19 +8,6 @@
  * Controller of the clientApp
  */
 angular.module('clientApp')
-  .controller('MoviesCtrl', function ($scope) {
-    $scope.movies = [
-      {
-        title: 'A New Hope',
-        url: 'https://www.youtube.com/watch?v=g6PDcBhODqo'
-      },
-      {
-        title: 'The Dark Knight Rises',
-        url: 'https://www.youtube.com/watch?v=gCEo7SCvYH4'
-      },
-      {
-        title: 'Inside Out',
-        url: 'https://www.youtube.com/watch?v=oauGmJ52XmM'
-      }
-    ];
+  .controller('MoviesCtrl', function ($scope, Movie) {
+    $scope.movies = Movie.getList().$object;
   });
